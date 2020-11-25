39028899822e147d6fad0992e6a5bd13f631920d
Sajeeb <sajeebahamed6@gmail.com>
2020-11-26 01:41:37 +0600

    custome validator created for register route

---
+++ validator/registerValidator.js
@@ -0,0 +1,31 @@
+const e = require('express')
+const validator = require('validator')
+const validate = user => {
+    let error = {}
+
+    if(!user.name) {
+        error.name = ' Please provide your name '
+    }
+    if(!user.email) {
+        error.email = 'Please provide your email'
+    } else if (!validator.isEmail(user.email)) {
+        error.email = 'Please provide valid email'
+    }
+
+    if(!user.password) {
+        error.password = 'Please provide a password'
+    } else if(user.password < 6) {
+        error.password = 'Passsword must be a greater than or Equal 6 Character'
+    }
+
+    if(!user.confirmPassword) {
+        error.confirmPassword = 'Please provide confirmation password'
+    } else if(user.password !== user.confirmPassword) {
+        error.confirmPassword = 'Password doesn\'t match'
+    }
+    return {
+        error,
+        isValid: Object.keys(error).length === 0
+    }
+}
+module.exports = validate
