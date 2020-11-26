beee30b1d5000075835abc8f04ca692fc2255845
Sajeeb <sajeebahamed6@gmail.com>
2020-11-27 03:34:54 +0600

    register route done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const registerValidator = require('../validator/registerValidator')
+const bcrypt = require('bcrypt');
+const User = require('../model/User');
 module.exports = {
     register(req, res) {
         let { name, email, password, confirmPassword } = req.body
@@ -7,13 +9,42 @@ module.exports = {
         if(!validate.isValid) {
             res.status(400).json(validate.error)
         } else {
-            res.status(200).json({
-                message: 'Everything is okay'
-            })
+            User.findOne({ email })
+                .then(user => {
+                    if(user => {
+                        return res.status(400).json({
+                            message: 'Email already exists'
+                        })
+                    })
+                    
+                    bcrypt.hash(password, 11, (err, hash) => {
+                        if(err) {
+                            return res.status(500).json({
+                                message: 'Server Error Occured'
+                            })
+                        }
+                        let user = new User({
+                            name,
+                            email,
+                            password: hash,
+                        })
+                        console.log(user);
+                        user.save()
+                        then(user => {
+                                res.status(201).json({
+                                    message: 'User Created Successfully',
+                                    user
+                                })
+                            })
+                            .catch(error => serverError(res, error))
+                    })
+                })
+                .catch(error => {
+                    console.log(error);
+                    res.status(500).json({
+                        message: 'Server Error Occured'
+                    })
+                })
         }
-        
-        res.json({
-            message: ` Hello ${name} and ${email} `
-        })
     }
 }
